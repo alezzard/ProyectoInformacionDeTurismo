35324783cbf819f30a81017a259dd79af532af0b
FrancoMCuello <franco.m.cuello@gmail.com>
2023-08-11 07:46:48 -0300

    adecuacion para la vinculacion con el back

---
+++ SantexAcademy/front/src/app/core/services/authService.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginUser } from 'src/app/models/login-user';
+import { Users } from 'src/app/models/users';
+import { JwtDto } from 'src/app/models/JwtDto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  authURL = 'https://localhost:3000';
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  public new(users: Users): Observable<any>{
+    return this.httpClient.post<any>(this.authURL + 'nuevo', users);
+  }
+
+  public login(loginUser: LoginUser): Observable<JwtDto>{
+    return this.httpClient.post<JwtDto>(this.authURL + 'login', loginUser);
+  }
+
+}
--- SantexAcademy/front/src/app/modules/login/login.module.ts
+++ SantexAcademy/front/src/app/modules/login/login.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { FormsModule } from '@angular/forms';
+import { LoginUser } from 'src/app/models/login-user';
+import { Users } from 'src/app/models/users';
 
 @NgModule({
   declarations: [
@@ -9,7 +12,8 @@ import { LoginPageComponent } from './login-page/login-page.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule,
   ],
   exports: [
     LoginPageComponent
