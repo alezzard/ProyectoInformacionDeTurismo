4b73746afde51f5ba20c2ef5d72a32588dbf127b
FrancoMCuello <franco.m.cuello@gmail.com>
2023-09-01 15:43:57 -0300

    se realizo la interfaz del componente encuesta

--- SantexAcademy/front/src/app/modules/encuesta/encuesta-page/encuesta-page.component.ts
+++ SantexAcademy/front/src/app/modules/encuesta/encuesta-page/encuesta-page.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import { TokenService } from 'src/app/core/services/token.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-encuesta-page',
   templateUrl: './encuesta-page.component.html',
-  styleUrls: ['./encuesta-page.component.css']
+  styleUrls: ['./encuesta-page.component.css'],
+  standalone: true,
+  imports: [
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule,
+    MatMenuModule
+  ],
 })
 export class EncuestaPageComponent {
+
+  constructor(private tokenService: TokenService, private router: Router){};
+
+  logout():void {
+    this.tokenService.logOut();
+    this.router.navigate(['/login']);
+  }
+
+  backHome():void {
+    this.router.navigate(['/home']);
+  }
 
 }
