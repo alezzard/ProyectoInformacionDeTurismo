a9623b42b1716da6420219f53a0fdd3adb93769b
AleGodoy <gabriel.alegodoy92@gmail.com>
2023-10-16 12:02:10 -0300

    configuración del registro de migrations y seeds funcionando

--- SantexAcademy/back/config/db-config.js
+++ SantexAcademy/back/config/db-config.js
@@ -1,14 +1,38 @@
 module.exports = {
   development: {
     dialect: "sqlite",
-    storage: "./database/.dev-database.sqlite"
+    storage: "./database/.dev-database.sqlite",
+    
+    // Configuración para seeders
+    seederStorage: "json",
+    seederStoragePath: "seeds.json", 
+
+    // Configuración para migrations
+    migrationStorage: "json",
+    migrationStorageTableName: "migrations.json"
   },
   test: {
     dialect: "sqlite",
-    storage: ":memory:"
+    storage: ":memory:",
+    
+    // Configuración para seedersnpx sequelize-cli
+    seederStorage: "json",
+    seederStoragePath: "seeds.json", 
+
+    // Configuración para migrations
+    migrationStorage: "json",
+    migrationStorageTableName: "migrations.json"
   },
   production: {
     dialect: "sqlite",
-    storage: "./database/.prod-database.sqlite"
+    storage: "./database/.prod-database.sqlite",
+    
+    // Configuración para seeders
+    seederStorage: "json",
+    seederStoragePath: "seeds.json", 
+
+    // Configuración para migrations
+    migrationStorage: "json",
+    migrationStorageTableName: "migrations.json"
   }
 };
